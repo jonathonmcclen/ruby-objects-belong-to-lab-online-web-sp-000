9e5ab9f5e528b653a27ded1f95e4a99f7006f0f2
jonathon McClendon <jonathonsmcclendon@gmail.com>
2021-05-30 18:54:36 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,16 +1,8 @@
 class Song 
-  
   attr_accessor :title, :artist
-  
-  def artist=(name)
-    @artist = Artist.new
-    @artist.name = name
-  end 
-  
 end 
 
+
 class Artist 
-  
   attr_accessor :name
-  
 end 
